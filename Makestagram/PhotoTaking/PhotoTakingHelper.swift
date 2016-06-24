0dcc6bfa53355276e80fbdd065f764d3a1e4d710
twof <fabiobean2@gmail.com>
2016-06-24 15:35:51 -0700

    Added phototakinghelper

---
+++ Makestagram/PhotoTaking/PhotoTakingHelper.swift
@@ -0,0 +1,56 @@
+//
+//  PhotoTakingHelper.swift
+//  Makestagram
+//
+//  Created by fnord on 6/24/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+
+typealias PhotoTakingHelperCallback = UIImage? -> Void
+
+class PhotoTakingHelper : NSObject {
+    
+    
+    
+    // View controller on which AlertViewController and UIImagePickerController are presented
+    weak var viewController: UIViewController!
+    var callback: PhotoTakingHelperCallback
+    var imagePickerController: UIImagePickerController?
+    
+    init(viewController: UIViewController, callback: PhotoTakingHelperCallback) {
+        self.viewController = viewController
+        self.callback = callback
+        
+        super.init()
+        
+        showPhotoSourceSelection()
+    }
+    
+    func showPhotoSourceSelection() {
+        // Allow user to choose between photo library and camera
+        let alertController = UIAlertController(title: nil, message: "Where do you want to get your picture from?", preferredStyle: .ActionSheet)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        
+        let photoLibraryAction = UIAlertAction(title: "Photo from Library", style: .Default) { (action) in
+            // do nothing yet...
+        }
+        
+        alertController.addAction(photoLibraryAction)
+        
+        // Only show camera option if rear camera is available
+        if (UIImagePickerController.isCameraDeviceAvailable(.Rear)) {
+            let cameraAction = UIAlertAction(title: "Photo from Camera", style: .Default) { (action) in
+                // do nothing yet...
+            }
+            
+            alertController.addAction(cameraAction)
+        }
+        
+        viewController.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+}
