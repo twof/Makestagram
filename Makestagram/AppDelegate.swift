2c2f2b9d39a700000ecdf298546f5b81a7f89e36
twof <fabiobean2@gmail.com>
2016-06-24 12:39:11 -0700

    Added classes and controllers to the project

--- Makestagram/AppDelegate.swift
+++ Makestagram/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,24 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
     // Override point for customization after application launch.
+    // Set up the Parse SDK
+    let configuration = ParseClientConfiguration {
+        $0.applicationId = "FirstApp"
+        $0.server = "https://makestagram-parse-arr.herokuapp.com/parse"
+    }
+    Parse.initializeWithConfiguration(configuration)
+    
+    do {
+        try PFUser.logInWithUsername("test", password: "test")
+    } catch {
+        print("Unable to log in")
+    }
+    
+    if let currentUser = PFUser.currentUser() {
+        print("\(currentUser.username!) logged in successfully")
+    } else {
+        print("No logged in user :(")
+    }
     
     return true
   }
